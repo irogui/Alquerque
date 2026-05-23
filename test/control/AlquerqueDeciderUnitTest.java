d976681ca465cf7a080a292026d0c2d1ddf1a40e
Guilian12 <guilian.leveque@edu.univ-fcomte.fr>
2026-05-24 01:34:48 +0200

    Add files via upload

---
+++ test/control/AlquerqueDeciderUnitTest.java
@@ -0,0 +1,13 @@
+package control;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class AlquerqueDeciderUnitTest {
+    @Test
+    public void testAlquerqueDecider() {
+        AlquerqueDecider alqDec = Mockito.mock(AlquerqueDecider.class);
+        Mockito.when(alqDec.decide()).thenReturn(null);
+        Mockito.when(alqDec.buildCapture(null, 0, 0, 1, 1, 1)).thenReturn(null);
+    }
+}
