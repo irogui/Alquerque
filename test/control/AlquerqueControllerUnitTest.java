c3276d648aaf53ecdd04ba1522e50eb836766be2
Guilian12 <guilian.leveque@edu.univ-fcomte.fr>
2026-05-24 01:38:13 +0200

    Add files via upload

---
+++ test/control/AlquerqueControllerUnitTest.java
@@ -0,0 +1,20 @@
+package control;
+
+import model.AlquerqueStageModel;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class AlquerqueControllerUnitTest {
+    @Test
+    public void testAlquerqueController() {
+        AlquerqueController alqCont = Mockito.mock(AlquerqueController.class);
+        Mockito.when(alqCont.analyseAndPlay(null)).thenReturn(false);
+        Mockito.when(alqCont.analyseAndPlay("test")).thenReturn(false);
+        Mockito.when(alqCont.analyseAndPlay("luigi")).thenReturn(false);
+        Mockito.when(alqCont.analyseAndPlay("B6-B5")).thenReturn(false);
+        Mockito.when(alqCont.analyseAndPlay("A1-A0")).thenReturn(false);
+        Mockito.when(alqCont.analyseAndPlay("D4-C3")).thenReturn(true);
+
+        /*Mockito.when(alqCont.hasAnyCapture(null, 1)).thenReturn(false);*/
+    }
+}
