9baf90af040e5d5e8abc234b6710b2aa848cef55
Guilian12 <guilian.leveque@edu.univ-fcomte.fr>
2026-05-25 01:47:32 +0200

    Implement unit tests for Pawn class
    Added unit tests for Pawn class to verify creation and type.

--- test/model/PawnUnitTest.java
+++ test/model/PawnUnitTest.java
@@ -1,4 +1,35 @@
 package model;
 
-public class PawnUnitTest {
+import boardifier.model.ElementTypes;
+import boardifier.model.GameStageModel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class PawnUnitTest {
+
+    @Test
+    void createPawnTest() {
+        GameStageModel stageModel = mock(GameStageModel.class);
+
+        /*Creating a white pawn.*/
+        Pawn Wpawn = new Pawn(Pawn.PAWN_WHITE, stageModel);
+
+        assertEquals(Pawn.PAWN_WHITE, Wpawn.getColor());
+
+        /*Creating a black pawn.*/
+        Pawn Bpawn = new Pawn(Pawn.PAWN_BLACK, stageModel);
+
+        assertEquals(Pawn.PAWN_BLACK, Bpawn.getColor());
+    }
+
+    @Test
+    void getTypeTest() {
+        GameStageModel stageModel = mock(GameStageModel.class);
+
+        Pawn pawn = new Pawn(Pawn.PAWN_WHITE, stageModel);
+
+        assertEquals(ElementTypes.getType("pion"), pawn.getType());
+    }
 }
